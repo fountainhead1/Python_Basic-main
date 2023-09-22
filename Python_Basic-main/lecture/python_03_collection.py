55ce0cd1375e0713c4a352bd41e1e5ad85cefab2
fountainhead1 <127369269+fountainhead1@users.noreply.github.com>
2023-09-22 12:38:15 +0900

    Add files via upload

--- Python_Basic-main/lecture/python_03_collection.py
+++ Python_Basic-main/lecture/python_03_collection.py
@@ -49,3 +49,105 @@ b = sorted(a)  # 원본 값은 정렬되지 않은 상태로 그대로 출력됨
 b = sorted(a, reverse=True)  # 내림차순 정렬도 가능함(Default : 오름차순)
 print(a)
 print(b)
+
+# 2. 튜플(Tuple) : 리스트와 대부분 동일, 정렬 불가능하고 immutable(생성 된 후 값 변경 불가능)
+# index 사용, packing/unpacking 가능, () 사용 - 생략 가능함
+# 직접 Tuple을 생성하는 경우는 드뭄 -> Python에서 결과값을 받을 때 Tuple로 제공함
+
+print("=" * 200)
+
+a = [1, 2, 3]  # List
+b = (1, 2, 3)  # Tuple
+c = 1, 2, 3  # Tuple(괄호를 생략 가능하다)
+
+a[0] = 99
+print(a)
+
+#  b[0] = 99
+#  print(b)
+#  오류 출력 -> Tuple은 값 변경이 불가능함
+
+# Tuple 원소가 1개인 경우, 1개의 값?
+a = (1, 2, 3)  # Tuple
+b = 1, 2, 3    # Tuple
+c = (1)        # Tuple
+d = 1          # Int(Tuple이 아닌 Int로 출력)
+e = 1,         # Tuple(원소 1개인 경우 원소 뒤에 , 를 붙여주면 Tuple로 출력)
+print(type(b))
+print(type(d))
+print(type(e))
+
+print("=" * 200)
+
+a = 5
+b = 8
+
+a, b = b, a  # 튜플(패킹) 활용
+
+print(a)
+print(b)
+
+# 3. Set(세트) : 수학의 집합 개념, 순서 없음(인덱스 및 정렬 불가능), 중복 값이 허용되지 않음(중요 -> 주 사용 목적), {} 사용
+# member 함수 : union(), intersection(), difference(), 등.. (합/차/교집합 개념)
+
+set_a = {1, 2, 3}
+set_b = {1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 5}  # 중복 값을 싹 지우고 출력 - {1, 2, 3, 4, 5}
+print(set_b)
+
+# e.g. 중복값 제거 활용 방법
+a = ["A", "A", "B", "B", "C", "C", "D", "E"]
+a = list(set(a))  # List -> Set(중복값 제거) -> List(원형 복구)
+
+print(a)
+
+# 4. 딕트(Dictionary) : 순서가 없음(인덱스, 정렬 X), {key : value} 꼴로 사용 - key, value가 1쌍
+# key는 중복 불가, value는 중복 가능, key를 통해서만 value에 접근이 가능함
+# member 함수 : update(), get(), keys(), values(), items()
+
+# 외부에서 데이터를 받아올 때 json이라는 형식으로 전달
+# json == dict (동일)
+
+dict_a = {"Korea" : "Seoul",
+          "Canada" : "Ottawa",
+          "USA" : "Washington D.C"}
+
+print(dict_a)
+
+# update() : dict와 dict를 병합해서 1개로 만듬(list의 extend와 동일)
+a = {"a" : 1,
+     "b" : 2}
+b = {"b" : 3,
+     "c" : 5}
+a.update(b)
+print(a)  # {'a': 1, 'b': 3, 'c': 5} : key값을 중복 허용하지 않음 -> (병합시 중복 key가 있는 경우) 나중에 온 값을 넣음
+
+# pop() : dict 원소를 key로 삭제
+
+c = a.pop("a")
+print(a)
+print(c)  # 삭제된 value를 출력
+
+# in() : () 안의 key 값이 존재하는지 확인
+print("c" in a)
+print("f" in a)
+
+# get() : 값 접근
+# list, tuple, dict 접근 -> 컬렉션[Index or Key] : ex) a[2]
+# print(a["f"])  # 존재하지 않는 key에 접근하는 경우 error 출력
+
+print(a.get("f"))  # key가 없으면 none 출력(error 출력 X)
+
+# keys(), values(), items() -> 사용빈도 높음
+print(a.keys())  # key만 추출
+print(a.values())  # value만 추출
+print(a.items())  # (key, value) 추출
+
+# 활용할 때 print(list(a.keys())) 같은 식으로 리스트로 형태 변경해서 추출
+
+# clear() : dict 초기화
+print(a)
+a.clear()
+print(a)
+
+a = {}
+print(type(a))  # <class 'dict'> : set 형태가 아닌 dict 형태

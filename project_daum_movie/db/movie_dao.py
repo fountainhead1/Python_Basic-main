a4e012f4572655fc1a081f84818013f4fa84dce9
fountainhead1 <127369269+fountainhead1@users.noreply.github.com>
2023-11-29 14:36:42 +0900

    Add files via upload

--- project_daum_movie/db/movie_dao.py
+++ project_daum_movie/db/movie_dao.py
@@ -12,7 +12,7 @@ def add_review(data):
     try:
         # 2) 일꾼 생성
         curs = conn.cursor()
-        # 3) 일거리 생성(SQL) → INSERT, DELETE, UPDATE, SELECT
+        # 3) 일거리 생성(SQL : 구조 질의어) → INSERT, DELETE, UPDATE(수정), SELECT(조회)
         sql = """
                INSERT INTO tbl_review(title, review, score, writer, reg_date)
                VALUES(%(title)s, %(review)s, %(score)s, %(writer)s, %(reg_date)s);

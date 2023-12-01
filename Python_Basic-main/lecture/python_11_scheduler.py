9a587c5e925d81520c873d0f51a8f41192714ed0
fountainhead1 <127369269+fountainhead1@users.noreply.github.com>
2023-12-01 11:30:17 +0900

    Add files via upload

--- Python_Basic-main/lecture/python_11_scheduler.py
+++ Python_Basic-main/lecture/python_11_scheduler.py
@@ -12,12 +12,28 @@
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 from datetime import datetime
+import time
 
+
+# print_today() 함수 : 호출하면 현재 시간을 출력해주는 함수
 def print_today():
     print(datetime.now())   # 현재 시간을 출력해주는 함수
+def print_love():
+    print("I Love You")
 
 #  1. 스케줄러 생성
 scheduler = BlockingScheduler()
 
+#  2. 스케줄러 일 할당(일, 주기, 5초)
+#    - date : 특정 날짜 및 시간에 1번만 동작
+#    - interval : 주기별로(5초, 10분, 1시간, ...)
+#    - CRON(★) : 만능(매일 언제~)
 scheduler.add_job(print_today, "interval", seconds=5)
+scheduler.add_job(print_love, "cron", hour="11", minute="29")
+
+#  3. 스케줄러 동작
 scheduler.start()
+
+# 임의의 work(background를 표기되게 하려면 이런 기능이 필요함)
+# while True:
+#   time.sleep(1)
